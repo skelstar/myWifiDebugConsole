1bcd52e69edce402598235968158b576aed07ca8
skelstar <skelstar@gmail.com>
2017-01-05 20:19:35 +1300

    Basically works... have to pass in list of lines to HTML-ise though.

--- myWifiDebugConsole.cpp
+++ myWifiDebugConsole.cpp
@@ -2,89 +2,41 @@
 #include <ESP8266WebServer.h>
 #include <myWifiDebugConsole.h>
 
-ESP8266WebServer _server (80);
-uint8_t _refreshSeconds;
+static ESP8266WebServer _server(80);
 
-myWifiDebugConsole::myWifiDebugConsole(uint8_t refreshSeconds)
+myWifiDebugConsole::myWifiDebugConsole()
 {
-	_refreshSeconds = refreshSeconds;
 }
-
-
-// void myWifiDebugConsole::Wrapper_To_Handle_Root() {
-// 	handleRoot();
-// }
 
 void myWifiDebugConsole::init() 
 {
-	char temp[400];
-    int sec = millis() / 1000;
-    int min = sec / 60;
-    int hr = min / 60;
-
-	snprintf ( temp, 400,
-
-"<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Debug Demo</title>\
-    <style>\
-      body { font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Timestamp: %02d:%02d:%02d</p>\
-  </body>\
-</html>",
-		//_refreshSeconds,
-        hr, 
-        min % 60, 
-        sec % 60
-    );
-
-
-    //_server.on ( "/", handleRoot );
-    _server.on( "/", [&]() 
-	{
-		_server.send ( 200, "text/plain", "temp");
-    });
-    //_server.on ( "/inline", []() {
-    //        _server.send ( 200, "text/plain", "this works as well" );
-    //    });
-    //server.onNotFound ( handleNotFound );
+    _server.on("/", handleRoot);
     _server.begin();
 }
 
-void myWifiDebugConsole::handleRoot() {
-/*
-    char temp[400];
+void myWifiDebugConsole::handleRoot() 
+{
+    char temp[600];
     int sec = millis() / 1000;
     int min = sec / 60;
     int hr = min / 60;
 
-    snprintf ( temp, 400,
-
+    snprintf(temp, 600,
 "<html>\
   <head>\
-    <meta http-equiv='refresh' content='5'/>\
+    <meta http-equiv='refresh' content='1'/>\
     <title>ESP8266 Debug Demo</title>\
-    <style>\
-      body { font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
   </head>\
   <body>\
     <h1>Hello from ESP8266!</h1>\
     <p>Timestamp: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
   </body>\
 </html>",
-		//_refreshSeconds,
         hr, 
         min % 60, 
         sec % 60
     );
-    _server.send ( 200, "text/html", temp );*/
+	_server.send(200, "text/html", temp);
 }
 
 void myWifiDebugConsole::handleClient()
--- myWifiDebugConsole.h
+++ myWifiDebugConsole.h
@@ -7,15 +7,12 @@
 class myWifiDebugConsole
 {
 	public:
-		myWifiDebugConsole(uint8_t refreshSeconds);
-		void handleClient();
+		myWifiDebugConsole();
 		void init();
+		void handleClient();
 
 	private:
-		ESP8266WebServer _server;
-		//void Wrapper_To_Handle_Root();
 		static void handleRoot();
-		uint8_t _refreshSeconds;
 };
 
 #endif
